49aa90eda7ced3fd1053d974fa507ae6c6206bca
jaemjeon <piepie1346@gmail.com>
2024-11-15 08:12:52 +0900

    edit config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-export default {
+module.exports = {
     reactStrictMode: true,
     api: {
-      bodyParser: false, // API 라우트에서 body parsing을 비활성화
+      bodyParser: false, // Disable body parsing for file uploads
     },
   };
