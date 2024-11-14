807988350539491ac1ee989899fc437ff858a31e
jaemjeon <piepie1346@gmail.com>
2024-11-15 08:08:32 +0900

    edit config

--- app/api/upload/route.js
+++ app/api/upload/route.js
@@ -1,21 +1,15 @@
-import { NextResponse } from 'next/server';
-import path from 'path';
 import { IncomingForm } from 'formidable';
 
+const form = new IncomingForm({
+  uploadDir: path.join(process.cwd(), 'public/uploaded-files'),
+  keepExtensions: true,
+});
 
-
-export async function POST(req) {
-    const form = new IncomingForm({
-        uploadDir: path.join(process.cwd(), 'public/uploaded-files'),
-        keepExtensions: true,
-      });
-
-  return new Promise((resolve, reject) => {
-    form.parse(req, async (err) => {
-      if (err) {
-        reject(new NextResponse('Error while uploading', { status: 500 }));
-      }
-      resolve(new NextResponse('File uploaded successfully', { status: 200 }));
-    });
-  });
-}
+form.parse(req, (err, fields, files) => {
+  if (err) {
+    console.error('File parsing error:', err);
+    return res.status(500).send('Error during file upload.');
+  }
+  // Handle the fields and files
+  res.status(200).json({ fields, files });
+});
